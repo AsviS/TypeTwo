ca9780bf1999258911b7d0d911bf3def9d03cfd4
MickeMakaron <MickeMakaron@users.noreply.github.com>
2015-09-09 01:38:55 +0200

    Header file for all WebSocket subprotocols

---
+++ GameServer/include/WebSocketSubProtocols.hpp
@@ -0,0 +1,11 @@
+#ifndef TYPETWO_WEBSOCKETSUBPROTOCOLS_HPP
+#define TYPETWO_WEBSOCKETSUBPROTOCOLS_HPP
+
+///////////////////////////////////
+// TypeTwo internal headers
+#include "WebSocketSubProtocol.hpp"
+///////////////////////////////////
+
+class OrderProtocol : public WebSocketSubProtocol{public:OrderProtocol();};
+
+#endif // TYPETWO_WEBSOCKETSUBPROTOCOLS_HPP

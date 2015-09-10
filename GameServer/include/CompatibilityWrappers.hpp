51d08231c0e6a1cad21773a41d47b563d664a4f1
MickeMakaron <MickeMakaron@users.noreply.github.com>
2015-09-10 21:01:02 +0200

    Wrappers for cross-compiler support

---
+++ GameServer/include/CompatibilityWrappers.hpp
@@ -0,0 +1,17 @@
+#ifndef TYPETWO_COMPATIBILITYWRAPPERS_HPP
+#define TYPETWO_COMPATIBILITYWRAPPERS_HPP
+
+///////////////////////////////////
+// STD C++
+#include <string>
+///////////////////////////////////
+
+/// \brief Convert string to char array
+///
+/// \param str const std::string&
+/// \return char*
+///
+///
+char* stringToChar(const std::string& str);
+
+#endif // TYPETWO_COMPATIBILITYWRAPPERS_HPP

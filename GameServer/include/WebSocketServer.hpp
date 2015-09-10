f463db2a0ba73be7478f4eff1385963c244a1965
MickeMakaron <MickeMakaron@users.noreply.github.com>
2015-09-10 04:10:15 +0200

    Added connection map and connection class.

--- GameServer/include/WebSocketServer.hpp
+++ GameServer/include/WebSocketServer.hpp
@@ -6,6 +6,12 @@
 #include "WebSocketSubProtocol.hpp"
 ///////////////////////////////////
 
+///////////////////////////////////
+// STD C++
+#include <map>
+///////////////////////////////////
+
+
 /// \brief WebSocket server
 ///
 /// Creates a WebSocket server that listens to
@@ -35,6 +41,28 @@ class WebSocketServer
         ///
         void run();
 
+        class ClientConnection
+        {
+            public:
+                ClientConnection(std::string username, std::string ip, const WebSocketServer& server) : mUsername(username), mIp(ip), mServer(server){};
+
+                std::string getUsername() const{return mUsername;};
+                std::string getIp() const{return mIp;};
+                const WebSocketServer& getServer() const{return mServer;};
+
+            private:
+                std::string mUsername;
+                std::string mIp;
+                const WebSocketServer& mServer;
+
+        };
+
+        void addClient(ClientConnection client);
+        void removeClient(std::string username);
+        const std::map<std::string, ClientConnection>& getClients() const;
+
+        bool userIsConnected(std::string username) const;
+
     private:
         /// \brief Initialize mProtocols member
         ///
@@ -45,9 +73,13 @@ class WebSocketServer
         void initializeProtocols(const std::vector<WebSocketSubProtocol::Ptr>& protocols);
 
 
+
     private:
+
+
         libwebsocket_context*   mContext;   ///< WebSocket context
         libwebsocket_protocols* mProtocols; ///< List of protocols to respond to
+        std::map<std::string, ClientConnection> mClients; ///< Clients connected to this server.
 };
 
 
---
+++ GameServer/include/WebSocketServers.hpp
@@ -0,0 +1,18 @@
+#ifndef TYPETWO_WEBSOCKETSERVERS_HPP
+#define TYPETWO_WEBSOCKETSERVERS_HPP
+
+///////////////////////////////////
+// TypeTwo internal headers
+#
+///////////////////////////////////
+
+class WebSocketServers
+{
+    public:
+
+
+    private:
+        std::map<libweb
+};
+
+#endif // TYPETWO_WEBSOCKETSERVERS_HPP

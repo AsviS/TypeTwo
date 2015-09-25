64f03821b2cdb693c188a91639f089417aa6005e
MickeMakaron <MickeMakaron@users.noreply.github.com>
2015-09-25 18:32:22 +0200

    Extended functionality of GUIText so that it handles font settings

--- Webb/controllers/typetwo/pages/game/typetwo-game/GUIText.js
+++ Webb/controllers/typetwo/pages/game/typetwo-game/GUIText.js
@@ -2,31 +2,64 @@
 
 var GUIText = function()
 {
-	function GUIText(text, position, lineGapThickness)
+	function GUIText(text, position, fontSettings)
 	{
 		this._text = text;
-		this._position = position;
-		this._lineGapThickness = lineGapThickness;
+		this.position = position || this.position;		
+		this.setFontSettings(fontSettings || GUIText.FontSettings.prototype);
 	}
+
+	GUIText.FontSettings = function(font, size, emphasis, color, baseLine)
+	{
+		this.font = 		font 		|| this.font;
+		this.size = 		size 		|| this.size;
+		this.emphasis = 	emphasis 	|| this.emphasis;
+		this.color = 		color 		|| this.color;
+		this.baseLine = 	baseLine 	|| this.baseLine;
+	};
+	
+	GUIText.FontSettings.prototype =
+	{
+		font: 'sans-serif',
+		size: '10',
+		emphasis: null,
+		color: 'black',
+		baseLine: 'top',
+		
+		toCanvasFontString: function()
+		{
+			return this.emphasis + " " + this.size + "px " + this.font;
+		}
+	};
 
 
 	$.extend(GUIText.prototype, GUIElement.prototype,
 	{
 		_text: [],
-		_position: new Vector(),
-		_lineGapThickness: 0,
+		position: new Vector(),
+		_fontSettings: null,
+		_canvasFontString: '',
+		_newlineStep: 0,
 		
 		render: function(ct)
 		{
-			ct.save();
-			var yPos = this._position.y;
-			ct.fillStyle = 'black';
+			ct.font = this._canvasFontString;
+			ct.fillStyle = this._fontSettings.color;
+			ct.textBaseline = this._fontSettings.baseLine;
+			
+			var yPos = this.position.y;
 			for(var i = 0; i < this._text.length; i++)
 			{
-				ct.fillText(this._text[i], this._position.x, yPos);
-				yPos += this._lineGapThickness;
+				ct.fillText(this._text[i], this.position.x, yPos);
+				yPos += this._newlineStep;
 			}
-			ct.restore();
+		},
+		
+		setFontSettings: function(fontSettings)
+		{
+			this._fontSettings = fontSettings;
+			this._canvasFontString = fontSettings.toCanvasFontString();
+			this._newlineStep = fontSettings.size * 3;
 		},
 	});
 

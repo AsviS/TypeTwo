12c5566316d5cfc474b95b30cff9c4432fb17031
MickeMakaron <MickeMakaron@users.noreply.github.com>
2015-09-30 16:13:03 +0200

    Implemented FetchDataProtocol and updated towards changes imposed by GeometricObject

--- Webb/controllers/typetwo/pages/game/typetwo-game/LoggedInMenuState.js
+++ Webb/controllers/typetwo/pages/game/typetwo-game/LoggedInMenuState.js
@@ -13,7 +13,7 @@ var LoggedInMenuState = function()
 			function(){stateStack.pop(); config.webSocket.order.disconnect(); stateStack.push(new LoginMenu(stateStack, canvas));}
 		);
 		
-		var text = new GUIText
+		this._text = new GUIText
 		([
 			"You are now logged in.",
 			"Gz!",
@@ -23,17 +23,54 @@ var LoggedInMenuState = function()
 			"the earliest-known supercontinent Rodinia, began to break apart. The continents later recombined to form Pannotia, ",
 			"600 to 540 million years ago, then finally Pangaea, which broke apart 180 million years ago.",
 		]);
-
-		this._guiContainer = new GUIContainer([logoutButton, text], new Rect(0, 0, this._width, this._height));
-
-		logoutButton.floatTo('center', 'center');
 		
-		text.floatTo('center');
-		text.moveY(this._height / 12);
+		var self = this;
+		var getUnits = function()
+		{
+			config.webSocket.fetchData.sendQuery("getUnits", function(response){self._text.setText(response.data);}, function(response){console.log("query timed out");});
+		};
+		
+		var getUnitsButton = new Button
+		(
+			new Rect(0, 0, this._width/3, this._height/10),
+			["Get units"],
+			getUnits
+		);
+		
+
+		this._guiContainer = new GUIContainer([logoutButton, this._text, getUnitsButton], new Rect(0, 0, this._width, this._height));
+
+		getUnitsButton.floatTo('center', 'center');
+		logoutButton.setPositionVector(getUnitsButton.getPosition());
+		logoutButton.moveY(2 * this._height/10);
+		
+		this._text.floatTo('center');
+		this._text.moveY(this._height / 12);
 		
 	}
 	
-	$.extend(LoggedInMenuState.prototype, MainMenuState.prototype);
+	$.extend(LoggedInMenuState.prototype, MainMenuState.prototype,
+	{
+		_text: null,
+		
+		update: function(dt)
+		{
+			this._guiContainer.update(dt);
+			
+			var messages = config.webSocket.fetchData.getMessages();
+			
+			for(var i = 0; i < messages.length; i++)
+			{
+				if(messages[i][0] === "getUnits")
+				{
+					console.log(messages[i]);
+					this._text.setText("yay!");
+				}
+			}
+			
+			config.webSocket.fetchData.swapMessagesBuffer();
+		},
+	});
 
 	return LoggedInMenuState;
 }();

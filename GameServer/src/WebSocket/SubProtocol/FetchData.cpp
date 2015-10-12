f2c8d3a42561918463c233421e591090cefe7987
MickeMakaron <MickeMakaron@users.noreply.github.com>
2015-10-12 14:34:47 +0200

    Implemented changes done on FetchDataProtocol and Standard streams

--- GameServer/src/WebSocket/SubProtocol/FetchData.cpp
+++ GameServer/src/WebSocket/SubProtocol/FetchData.cpp
@@ -5,7 +5,7 @@
 #include "WebSocket/Server.hpp"
 #include "Database/StoredProcedures.hpp"
 #include "WebSocket/Connection.hpp"
-#include "Database/FetchDataProtocolStream.hpp"
+#include "Database/StreamFetchDataProtocol.hpp"
 using namespace WebSocket;
 ///////////////////////////////////
 
@@ -49,35 +49,34 @@ const SubProtocol& SubProtocols::FETCH_DATA = SubProtocol
             Connection& connection = SubProtocol::getConnection(connectionData);
 
             typedef Database::StoredProcedures SP;
-            using namespace Database;
-
+            namespace Stream = Database::Stream::FetchDataProtocol;
 
             if(procedure == "getUnitsByZoneId")
             {
                 int zoneId;
                 message >> zoneId;
-                std::string units = FetchDataProtocol::call(SP::GET_UNITS_BY_ZONE_ID, zoneId);
+                std::string units = Stream::call(SP::GET_UNITS_BY_ZONE_ID, zoneId);
                 connection.sendString(id + '\n' + units);
             }
             else if(procedure == "getUnits")
             {
                 int userId;
-                Database::StoredProcedures::GET_USER_ID.call(connection.getUsername(), userId);
-                std::string units = FetchDataProtocol::call(SP::GET_UNITS, userId);
+                SP::GET_USER_ID.call(connection.getUsername(), userId);
+                std::string units = Stream::call(SP::GET_UNITS, userId);
                 connection.sendString(id + '\n' + units);
             }
             else if(procedure == "getUnitTypes")
             {
-                connection.sendString(id + '\n' + FetchDataProtocol::call(SP::GET_ALL_UNIT_TYPES));
+                connection.sendString(id + '\n' + Stream::call(SP::GET_ALL_UNIT_TYPES));
             }
             else if(procedure == "getVisibleUnits")
             {
                 int userId;
-                Database::StoredProcedures::GET_USER_ID.call(connection.getUsername(), userId);
+                SP::GET_USER_ID.call(connection.getUsername(), userId);
 
-                std::vector<int> zoneIds = Database::StoredProcedures::GET_USER_ZONE_IDS.call<int>(userId);
+                std::vector<int> zoneIds = SP::GET_USER_ZONE_IDS.call<int>(userId);
 
-                auto stream = Database::FetchDataProtocol::createStream(Database::StoredProcedures::GET_UNITS_BY_ZONE_ID);
+                auto stream = Stream::create(SP::GET_UNITS_BY_ZONE_ID);
                 for(int zoneId : zoneIds)
                     stream.execute(zoneId);
 
